afd786f2d424645a4e19afb2722f6941e875fa68
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-09-09 17:11:27 +0700

    Структура папок

---
+++ src/components/NavigatePanel/NavigatePanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NavigatePanel.scss'
+
+const NavigatePanel = () => {
+  return (
+    <div>
+      NavigatePanel
+    </div>
+  )
+}
+
+export default NavigatePanel
---
+++ src/components/Registration/Registration.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Registration.scss'
+
+const Registration = () => {
+  return (
+    <div>
+      Registration
+    </div>
+  )
+}
+
+export default Registration
