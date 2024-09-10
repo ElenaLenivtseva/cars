4f8a70667d6d5e9aee26f0939ac830c3b4442386
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-09-10 12:59:09 +0700

    header, navigatepanel и стили к ним

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import News from '../../components/News/News'
+import NavigatePanel from '../../components/NavigatePanel/NavigatePanel'
 
 const NewsPage = () => {
   return (
     <div className='container'>
+      <NavigatePanel/>
       <News/>
     </div>
   )
