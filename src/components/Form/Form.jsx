aa6c5df9c11a382435615dff34068cc5fb13c53a
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-09-11 14:41:02 +0700

    работающая версия валидации и блокировки/разблокровки кнопки отправки

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -16,46 +16,112 @@ function isEmailValid(value) {
 const Form = () => {
   const [form, setForm] = useState(initialForm);
   const [disabled, setDisabled] = useState(true);
-  const [loginError, setLoginError] = useState("ллл");
-  const [emailError, setEmailError] = useState("прпоп");
-  const [passwordError, setPasswordError] = useState("прарпа");
+  const [loginError, setLoginError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [allError, setAllError] = useState({
+    login: true,
+    email: true,
+    password: true,
+  });
 
   function checkValidate(name, value) {
+    
     switch (name) {
       case "login":
-        if (value.length > 4) {
-          setLoginError(false);
-          return true;
-        } else {
+        if (value.length < 2) {
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.login = true
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
+          
+        } else if (value.length > 2 &&value.length < 5){
           setLoginError("Длина логина должна быть не менее 5 символов");
+          
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.login = true
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
           return false;
-        }
-
+        } else if (value.length>4){
+          setLoginError("");
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.login = false
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
+          return true;
+        } 
+        break;
       case "password":
-        if (value.length > 6) {
-          setPasswordError(false);
+        if (value.length < 2) {
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.password = true;
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
+    
+        } else if (value.length > 7) {
+          setPasswordError("");
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.password = false
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
           return true;
         } else {
           setPasswordError("Длина пароля должна быть не менее 8 символов");
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.password = true
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
           return false;
         }
-
+        break;
       case "email":
-        if (isEmailValid(value)) {
+        if (value.length < 2) {
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.email = true
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
+        } else if (isEmailValid(value)) {
           setEmailError("");
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.email = false
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
           return true;
         } else {
           setEmailError(
             "Проверьте правильность формата введенного email-адреса"
           );
+          setAllError(prevAllError => {
+            const updatedErrors = { ...prevAllError };
+            updatedErrors.email = true
+            everythingIsValid(updatedErrors); 
+            return updatedErrors;
+          });
           return false;
         }
-
+        break;
       default:
         alert("Нет таких значений");
     }
+    
   }
-  
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -63,24 +129,19 @@ const Form = () => {
       ...prevFormData,
       [name]: value,
     }));
-    
-    if (value.length > 1) {
-      checkValidate(name, value);
-      everythingIsValid()
-    }
-    
-    
+
+    checkValidate(name, value);
   };
-  function everythingIsValid() {
-    let anyErrorExist = loginError || emailError || passwordError;
-    console.log(anyErrorExist);
-    if(loginError || emailError || passwordError){
+
+  function everythingIsValid(updated) {
+    const { login, email, password } = updated;
+    const anyErrorExist = login || email || password;
+    if(anyErrorExist){
       setDisabled(true)
     } else {
       setDisabled(false)
     }
   }
-  
 
   return (
     <div className="form__container">
@@ -92,41 +153,44 @@ const Form = () => {
           <label className="form__label">
             Логин
             <input
-              className="form__input"
+              className={`form__input ${loginError&&'form__input_error'}`}
+              
               name="login"
               type="text"
               placeholder="Придумайте логин"
               value={form.login}
               onChange={(e) => {
                 handleChange(e);
-                
               }}
             />
           </label>
-          <p className="form__error">{loginError}</p>
+          <p className='form__error'>{loginError}</p>
+         
         </div>
         <div className="form__labelWrap form__email">
           <label className="form__label">
             Email
             <input
-              className="form__input"
+              className={`form__input ${emailError&&'form__input_error'}`}
+             
               name="email"
               type="email"
               placeholder="Введите адрес электронной почты"
               value={form.email}
               onChange={(e) => {
                 handleChange(e);
-                
               }}
             />
           </label>
-          <p className="form__error">{emailError}</p>
+         
+          <p className='form__error'>{emailError}</p>
         </div>
         <div className="form__labelWrap form__password">
           <label className="form__label">
             Пароль
             <input
-              className="form__input"
+              className={`form__input ${passwordError&&'form__input_error'}`}
+             
               name="password"
               type="password"
               placeholder="Придумайте пароль"
@@ -136,7 +200,7 @@ const Form = () => {
               }}
             />
           </label>
-          <p className="form__error">{passwordError}</p>
+          <p className='form__error'>{passwordError}</p>
         </div>
         <div className="form__buttonWrap">
           <button className="form__button" disabled={disabled}>
